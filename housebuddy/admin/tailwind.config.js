32fd0b7c26a68983c52f344c9f83e3b221b9ac8c
Suraj-Shendge <124484652+Suraj-Shendge@users.noreply.github.com>
2026-03-13 22:04:15 +0530

    Update tailwind.config.js

--- housebuddy/admin/tailwind.config.js
+++ housebuddy/admin/tailwind.config.js
@@ -1,1 +1,12 @@
-module.exports = {};
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#5B8DEF',
+        accent: '#00C2A8',
+      },
+    },
+  },
+  plugins: [],
+};
