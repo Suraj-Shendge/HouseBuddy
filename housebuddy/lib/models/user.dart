1117f0eb33fdfef10556e17579bff8ee5c4c3d24
Suraj-Shendge <124484652+Suraj-Shendge@users.noreply.github.com>
2026-03-13 21:51:42 +0530

    Update user.dart

--- housebuddy/lib/models/user.dart
+++ housebuddy/lib/models/user.dart
@@ -1,1 +1,18 @@
-// User model
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'user.freezed.dart';
+part 'user.g.dart';
+
+@freezed
+class User with _$User {
+  const factory User({
+    required String uid,
+    required String name,
+    required String phone,
+    @Default('customer') String role, // "customer" | "provider"
+    required DateTime createdAt,
+    GeoPoint? location,
+  }) = _User;
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+}
