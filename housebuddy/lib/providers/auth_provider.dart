49c458743eeaf414037aff5114768eac097af3cb
Suraj-Shendge <124484652+Suraj-Shendge@users.noreply.github.com>
2026-03-13 21:53:18 +0530

    Update auth_provider.dart

--- housebuddy/lib/providers/auth_provider.dart
+++ housebuddy/lib/providers/auth_provider.dart
@@ -1,1 +1,34 @@
-// Authentication provider
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../models/user.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final firebaseAuthProvider = Provider<FirebaseAuth>((ref) => FirebaseAuth.instance);
+final firestoreProvider = Provider<FirebaseFirestore>((ref) => FirebaseFirestore.instance);
+
+final firebaseUserProvider = StreamProvider<User?>((ref) {
+  return ref.read(firebaseAuthProvider).authStateChanges();
+});
+
+final appUserProvider = FutureProvider<UserModel?>((ref) async {
+  final firebaseUser = await ref.watch(firebaseUserProvider.future);
+  if (firebaseUser == null) return null;
+  final snap = await ref
+      .read(firestoreProvider)
+      .collection('users')
+      .doc(firebaseUser.uid)
+      .get();
+  if (!snap.exists) {
+    // First time login – create user doc
+    final newUser = UserModel(
+      uid: firebaseUser.uid,
+      name: '',
+      phone: firebaseUser.phoneNumber ?? '',
+      role: 'customer',
+      createdAt: DateTime.now(),
+    );
+    await snap.reference.set(newUser.toJson());
+    return newUser;
+  }
+  return UserModel.fromJson(snap.data()!);
+});
