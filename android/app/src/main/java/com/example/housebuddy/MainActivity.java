2452bb153cf943d70c33c43d1f290499fccda797
Suraj-Shendge <124484652+Suraj-Shendge@users.noreply.github.com>
2026-03-14 02:39:15 +0530

    Update MainActivity.java

--- android/app/src/main/java/com/example/housebuddy/MainActivity.java
+++ android/app/src/main/java/com/example/housebuddy/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.housebuddy;
 import io.flutter.embedding.android.FlutterFragmentActivity;
 
 /**
- * Updated to use the new Android embedding (v2).
+ * Updated to use the new Android v2 embedding.
  */
 public class MainActivity extends FlutterFragmentActivity {
 }
